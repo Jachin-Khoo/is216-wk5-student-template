d551d933e17292ec3da2972a1da0f268baa5d089
Jachin-Khoo <jachinkhoo@gmail.com>
2026-09-14 09:29:45 +0800

    koh kai sen

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -1,59 +1,67 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 
 const teamA = ref("Falcons")
 const teamB = ref("Tigers")
-const scoreA = ref(0)
+const scoreA = ref(0)   
 const scoreB = ref(0)
 const step = ref(1) // points added per click
 const maxScore = ref(10)
 
-const gameOver = computed(() => scoreA.value >= maxScore.value || scoreB.value >= maxScore.value)
-
 function addA() {
-  scoreA.value = Math.min(maxScore.value, scoreA.value + step.value)
+    scoreA.value = Math.min(maxScore.value, scoreA.value + step.value);
 }
 
 function addB() {
-  scoreB.value = Math.min(maxScore.value, scoreB.value + step.value)
+    scoreB.value = Math.min(maxScore.value, scoreB.value + step.value);
 }
 
 function reset() {
-  scoreA.value = 0
-  scoreB.value = 0
+    scoreA.value = 0;
+    scoreB.value = 0;
 }
+
 </script>
 
 <template>
-  <div style="font-family: Arial; max-width: 520px; margin: 24px auto;">
-    <h2>Mini Scoreboard</h2>
 
-    <p><strong>{{ teamA }}</strong> vs <strong>{{ teamB }}</strong></p>
+    <div style="font-family: Arial; max-width: 520px; margin: 24px auto;">
+        <h2>Mini Scoreboard</h2>
 
-    <p>Current: {{ scoreA }} - {{ scoreB }}</p>
+        <p><strong>{{ teamA }}</strong> vs <strong>{{ teamB }}</strong></p>
 
-    <p>Total points: {{ scoreA + scoreB }}</p>
-    <p>Points left to win: {{ maxScore - Math.max(scoreA, scoreB) }}</p>
+        <p>Current: {{ scoreA }} - {{ scoreB }}</p>
 
-    <div style="display: flex; gap: 12px; margin: 12px 0;">
-      <button type="button" @click="addA">+ Team A</button>
-      <button type="button" @click="addB">+ Team B</button>
-      <button type="button" @click="reset">Reset</button>
+        <!-- B. In-template expressions go here -->
+        <p>Total points: {{ scoreA + scoreB }}</p>
+        <p>Points left to win: {{ 10 - Math.max(scoreA, scoreB) }}</p>
+        <!-- A. Event handlers go here -->
+        <div style="display: flex; gap: 12px; margin: 12px 0;">
+            <button @click="addA">+ Team A</button>
+            <button @click="addB">+ Team B</button>
+            <button @click="reset">Reset</button>
+        </div>
+
+
+        <div style="margin-top: 14px;">
+            <!-- C. Display winner / status here -->
+            <p v-if="scoreA == maxScore">Winner: {{ teamA.toUpperCase()}}</p>
+            <p v-else-if="scoreB == maxScore">Winner: {{ teamB.toUpperCase()}}</p>
+            <p v-else>No winner yet. Keep playing!</p>
+        </div>
+
+
     </div>
 
-    <div style="margin-top: 14px;">
-      <p v-if="gameOver">Winner: {{ scoreA > scoreB ? teamA : teamB }}</p>
-    </div>
-  </div>
 </template>
 
 <style scoped>
 p,
 input {
-  font-family: monospace;
+    font-family: monospace;
 }
 
 p {
-  white-space: pre;
+    white-space: pre;
 }
 </style>
